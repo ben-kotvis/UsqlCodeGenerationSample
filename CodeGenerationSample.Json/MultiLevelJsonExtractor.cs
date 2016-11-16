7bfa907da22679a1fe16e6e35e6be2e2703d4892
Ben Kotvis <kotvisbj@gmail.com>
2016-11-16 13:41:45 -0600

    Moving to GitHub

---
+++ CodeGenerationSample.Json/MultiLevelJsonExtractor.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Analytics.Interfaces;
+using Newtonsoft.Json.Linq;
+using CodeGenerationSample.Json.Exceptions;
+
+namespace CodeGenerationSample.Json
+{
+    public class MultiLevelJsonExtractor : JsonExtractor
+    {
+        private string[] jsonPaths;
+        private readonly bool bypassWarning;
+
+        /// <summary/>
+        public MultiLevelJsonExtractor(string rowpath = null, bool bypassWarning = false, params string[] jsonPaths)
+            :base(rowpath)
+        {
+            this.jsonPaths = jsonPaths;
+            this.bypassWarning = bypassWarning;
+        }
+
+        protected override void JObjectToRow(JObject o, IUpdatableRow row)
+        {
+            if (jsonPaths.Length == 0)
+            {
+                base.JObjectToRow(o, row);
+            }
+            else
+            {
+                for (int i = 0; i < jsonPaths.Length; i++)
+                {
+                    var path = jsonPaths[i];
+                    var jObj = findByPath(o, path);
+                    object value = null;
+
+                    if (jObj != default(JToken))
+                    {
+                        var schemaColumn = row.Schema[i];
+                        value = JsonFunctions.ConvertToken(jObj, schemaColumn.Type) ?? schemaColumn.DefaultValue;
+                    }
+                    row.Set<object>(i, value);                    
+                }
+            }
+        }
+
+        /// <summary>
+        /// Recursively walks up the tree to find a path if it wasn't found at the device level
+        /// </summary>
+        /// <param name="o"></param>
+        /// <param name="path"></param>
+        /// <returns></returns>
+        private JToken findByPath(JToken o, string path)
+        {
+            var jObj = o.SelectToken(path);
+
+            if(jObj == null)
+            {
+                if(o.Parent == null || !(o.Parent is JToken))
+                {
+                    if(bypassWarning)
+                    {
+                        return default(JObject);
+                    }
+                    throw new PathNotFoundException(string.Format("Path {0} could not be found!", path));
+                }
+
+                return findByPath(o.Parent, path);
+            }
+
+            return jObj;
+        }
+    }
+}
---
+++ CodeGenerationSample.Scripts.Test/JsonExtractorTest.cs
@@ -0,0 +1,115 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using Microsoft.Analytics.UnitTest;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeGenerationSample.Json;
+using CodeGenerationSample.Json.Exceptions;
+
+namespace CodeGenerationSample.Scripts.Test
+{
+    [TestClass]
+    public class JsonExtractorTest
+    {
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+        
+        public IRow RowGenerator()
+        {
+            //generate the schema
+            USqlColumn<string> col1 = new USqlColumn<string>("Region");
+            USqlColumn<string> col2 = new USqlColumn<string>("MarketName");
+            USqlColumn<string> col3 = new USqlColumn<string>("MarketId");
+            USqlColumn<string> col4 = new USqlColumn<string>("Produce");
+            List<IColumn> columns = new List<IColumn> { col1, col2, col3, col4 };
+            USqlSchema schema = new USqlSchema(columns);
+            return new USqlRow(schema, null);
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"InputFiles\MarketLocations.json")]
+        public void TestAdvancedPaths()
+        {
+            IUpdatableRow output = RowGenerator().AsUpdatable();
+
+            using (FileStream stream = new FileStream(@"MarketLocations.json", FileMode.Open))
+            {
+                //Read input file 
+                USqlStreamReader reader = new USqlStreamReader(stream);
+                //Run the UDO
+                JsonExtractor extractor = new MultiLevelJsonExtractor("MarketLocations[*]", false, "Region", "MarketName", "MarketId", "SalesCategories.Produce");
+                List <IRow> result = extractor.Extract(reader, output).ToList();
+                //Verify the schema
+                Assert.IsTrue(result.Count == 3);
+                Assert.IsTrue(result[0].Schema.Count == 4);
+                //Verify the result
+                Assert.IsTrue(result[0].Get<string>("MarketName") == "Central");
+                Assert.IsTrue(result[0].Get<string>("Produce") != null);
+            }
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"InputFiles\MarketLocations.json")]
+        public void TestBypassWarning()
+        {
+            IUpdatableRow output = RowGenerator().AsUpdatable();
+
+            using (FileStream stream = new FileStream(@"MarketLocations.json", FileMode.Open))
+            {
+                //Read input file 
+                USqlStreamReader reader = new USqlStreamReader(stream);
+
+                //Run the UDO
+                JsonExtractor extractor = new MultiLevelJsonExtractor("MarketLocations[*]", true, "RegionBogus", "MarketName", "MarketId", "SalesCategories.Produce");
+
+                try
+                {
+                    List<IRow> result = extractor.Extract(reader, output).ToList();
+                }
+                catch(PathNotFoundException)
+                {
+                    Assert.Fail("Exception shouldn't be thrown when bypassWarning is set to true");
+                }
+            }
+        }
+
+        [TestMethod]
+        [DeploymentItem(@"InputFiles\MarketLocations.json")]
+        [ExpectedException(typeof(PathNotFoundException))]
+        public void TestBadPathException()
+        {
+
+            IUpdatableRow output = RowGenerator().AsUpdatable();
+
+            using (FileStream stream = new FileStream(@"MarketLocations.json", FileMode.Open))
+            {
+                //Read input file 
+                USqlStreamReader reader = new USqlStreamReader(stream);
+
+                //Run the UDO
+                JsonExtractor extractor = new MultiLevelJsonExtractor("MarketLocations[*]", false, "RegionBogus", "MarketName", "MarketId", "SalesCategories.Produce");
+                List<IRow> result = extractor.Extract(reader, output).ToList();
+            }
+        }
+
+    }
+}
